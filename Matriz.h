ba2b78378c6d1c2271ce5523968b5d8918618cd0
FranciscoCalel <36779013+FranciscoCalel@users.noreply.github.com>
2020-03-11 19:21:00 -0600

    matriz
    creando matriz y metodos a utilizar

---
+++ Matriz.h
@@ -0,0 +1,45 @@
+#ifndef MATRIZ
+#define MATRIZ
+
+class Nodo_Matriz{
+	public:
+		int fila, columna;
+		char * valor;
+		
+		NodoMatriz *izq;
+		NodoMatriz *der;
+		NodoMatriz *arriba;
+		NodoMatriz *abajo;
+		NodoMatriz(int fila, int columna, char*valor);
+		
+};
+
+class Guia{
+
+	public:
+		int id;
+		Guia * sig;
+		Guia * ant;
+		NodoMatriz * entrada;
+		guia(int id);
+}
+
+class Lista_guias{
+	Guia * prim;
+	void insert(Guia * nuevo);
+	Guia * getGuia(int id);
+};
+
+class Matriz{
+	Lista_Guias * Nfilas;
+	Lista_Guias * NColumnas;
+	Matriz();
+	void insertar(int fil, int columna, char * valor);
+	void recFil();
+	void recCol();
+	void eliminar(int fila, int columna)
+	void rep();
+	NodoMatriz * buscar(int fila,int columna);	
+};
+
+#endif
