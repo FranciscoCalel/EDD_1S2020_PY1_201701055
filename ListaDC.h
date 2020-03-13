4f88d4b5f8399b68706e04d0b2e0d80ae0a1af8b
FranciscoCalel <36779013+FranciscoCalel@users.noreply.github.com>
2020-03-12 23:45:47 -0600

    lista doble circular
    EDD OTRA

---
+++ ListaDC.h
@@ -0,0 +1,34 @@
+#ifndef LISTADOBLECIRCULAR_H
+#define LISTADOBLECIRCULAR_H
+#include <iostream>
+//#include <fstream>
+#include  <string.h>
+using namespace std;
+
+typedef class nodold nodold;
+typedef class ListaDC ListaDC;
+
+class nodold
+{
+	public: 
+		string palabra;
+		nodold *sig;
+		nodold *ant;
+		nodold(string palabra);
+
+	
+};
+class ListaDC
+{
+	public:
+		nodold *ultimo;
+		nodold *primero;
+		int tamaLC;
+		ListaDC();
+		void insertar(string palabra);
+		void print();
+		void reporte();
+	protected:
+};
+
+#endif
