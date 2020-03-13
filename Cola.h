f4a8789314efc8a9574dcd3686b5763e9f407f09
FranciscoCalel <36779013+FranciscoCalel@users.noreply.github.com>
2020-03-12 23:34:54 -0600

    cola
    una estructura mas

---
+++ Cola.h
@@ -0,0 +1,30 @@
+#ifndef COLA_H
+#define COLA_H
+typedef class nodoCola nodoCola;
+typedef class Cola Cola;
+
+class nodoCola
+{
+	public:
+		char *letra;
+		int punteo;
+		nodoCola *sig;
+		nodoCola(char *letra, int punteo);
+	
+	
+};
+class Cola
+{
+	public:
+		nodoCola * primero;
+		nodoCola *ultimo;
+		int tamC;
+		Cola();
+		void insertar(char *letra, int punteo);
+		void eliminar();
+		void reporte();
+		void print();
+	protected:
+};
+
+#endif
